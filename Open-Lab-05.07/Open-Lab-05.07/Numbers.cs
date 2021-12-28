8ee92b75e3793ecfd83a76a51c2402d32d0420ae
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-28 20:31:46 +0100

    removes first smallest number from integer array

---
+++ Open-Lab-05.07/Open-Lab-05.07/Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Open_Lab_05._07
+{
+    public class Numbers
+    {
+        public int[] RemoveSmallest(int[] nums)
+        {
+            int smallest = 999999999;
+            int pos = 0;
+            
+            int pos3 = 0;
+            
+            foreach (int i in nums)
+            {
+                pos++;
+                if (i < smallest)
+                {
+                    smallest = i;
+                    
+                }
+
+            }
+            int[] final = new int[pos-1];
+            foreach (int i in nums)
+            {
+                
+                if (i > smallest)
+                {
+                    final[pos3] = i;
+                    pos3++;
+                }
+                else
+                {
+                    smallest = -999999999;
+                }
+
+            }
+
+            return final;
+        }
+    }
+}
